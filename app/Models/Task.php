31fc546b2a7ee04905e168a6642d54b0bd1725f3
ShotaMihara <kreva.biz8354@gmail.com>
2025-03-25 02:01:15 +0900

    taskテーブルにorderカラム追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -15,6 +15,7 @@ class Task extends Model
         'description',
         'deadline',
         'status',
+        'order',
     ];
 
     public function goal()
--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -12,6 +12,21 @@ class TaskFactory extends Factory
 {
 
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\Task::class;
+
+    /**
+     * The current order value.
+     *
+     * @var int
+     */
+    protected static $order = 1;
+
+    
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
@@ -24,6 +39,8 @@ class TaskFactory extends Factory
             'description' => $this->faker->realText(50),
             'deadline' => $this->faker->dateTimeBetween('now', '+1 month'),
             'status' => $this->faker->numberBetween(0, 1),
+            'order' => self::$order++,
+            
 
         ];
     }
