f5bd073ddae57b616c17a53b008b5b7a824363c8
ShotaMihara <kreva.biz8354@gmail.com>
2025-04-06 17:35:34 +0900

    mypage コンテンツマージン調整

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -22,14 +22,23 @@
             <div class="mb-4 text-lg text-gray-600 dark:text-gray-400 w-full max-w-4xl">
                 @foreach ($goals as $goal)
                     <div class="bg-white dark:bg-gray-800 shadow overflow-hidden sm:rounded-lg mb-4 p-4 border border-transparent hover:border-white">
-                        <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200">
+                        <h2 class="mt-2 text-2xl font-semibold text-gray-800 dark:text-gray-200">
                             {{ $goal->title }}
                         </h2>
                         <div class="text-gray-600 dark:text-gray-400">
-                            <span>進捗度：{{ $goal->progress }}%</span>
-                            <span>達成期限：{{ $goal->deadline }}</span>
-                            <span>{{ $goal->is_completed ? '達成済み' : '未達成' }}</span><br>
-                            <a href="{{ route('show', $goal->id) }}" class="inline-flex items-center mt-4 px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">詳細を見る</a>
+                            <div class="mt-4">
+                                <p class="text-lg font-medium">
+                                    進捗度: <span class="text-blue-500">{{ $goal->progress }}%</span>
+                                </p>
+                                <div class="w-full bg-gray-300 rounded-full h-4 mt-2">
+                                    <div class="bg-blue-500 h-4 rounded-full" style="width: {{ $goal->progress }}%;"></div>
+                                </div>
+                                <p class="mt-2 mb-2">ステータス：{{ $goal->progress == 100 ? '達成済み' : '未達成' }}</ｐ><br>
+                                <a href="{{ route('show', $goal->id) }}" class="inline-flex items-center mt-2 px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                    詳細を見る
+                                </a>
+                            </div>
+                            
                         </div>
                     </div>
                 @endforeach
