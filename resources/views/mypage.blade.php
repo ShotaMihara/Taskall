ff944689e0c67e6c7abd5e94c0ab0552a8bc3f1d
ShotaMihara <kreva.biz8354@gmail.com>
2025-04-06 17:08:07 +0900

    mypageに詳細ボタン追加

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -23,12 +23,13 @@
                 @foreach ($goals as $goal)
                     <div class="bg-white dark:bg-gray-800 shadow overflow-hidden sm:rounded-lg mb-4 p-4 border border-transparent hover:border-white">
                         <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-200">
-                            <a href="{{ route('show', $goal->id) }}" class="hover:text-blue-500">{{ $goal->title }}</a>
+                            {{ $goal->title }}
                         </h2>
                         <div class="text-gray-600 dark:text-gray-400">
                             <span>進捗度：{{ $goal->progress }}%</span>
                             <span>達成期限：{{ $goal->deadline }}</span>
-                            <span>{{ $goal->is_completed ? '達成済み' : '未達成' }}</span>
+                            <span>{{ $goal->is_completed ? '達成済み' : '未達成' }}</span><br>
+                            <a href="{{ route('show', $goal->id) }}" class="inline-flex items-center mt-4 px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">詳細を見る</a>
                         </div>
                     </div>
                 @endforeach
