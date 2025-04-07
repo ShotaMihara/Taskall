afb7277963343422fa3f278749b136bed9e254d0
ShotaMihara <kreva.biz8354@gmail.com>
2025-04-07 22:03:19 +0900

    auth関連まとめ

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,27 @@ use App\Http\Controllers\GoalController;
 use App\Http\Controllers\GeminiController;
 use App\Http\Controllers\TaskController;
 
-// TOPページ
+// TOPページ（認証不要）
 Route::get('/', function () {
     return view('top');
 });
 
+// 認証が必要なルート
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/mypage', [GoalController::class, 'GoalIndex'])->name('mypage');
+    Route::get('/goal/{id}', [GoalController::class, 'GoalShow'])->name('show');
+    Route::get('/ask', [GeminiController::class, 'askQuestion'])->name('ask');
+    Route::view('/setting', 'setting')->name('setting');
 
-Route::get('/mypage', [GoalController::class, 'GoalIndex'])->middleware(['auth', 'verified'])->name('mypage');
-Route::get('/goal/{id}', [GoalController::class, 'GoalShow'])->middleware(['auth', 'verified'])->name('show');
-Route::get('/ask', [GeminiController::class, 'askQuestion'])->middleware(['auth', 'verified'])->name('ask');
-Route::view('/setting', 'setting')->middleware(['auth', 'verified'])->name('setting');
+    Route::post('/save-to-database', [GeminiController::class, 'saveToDatabase'])->name('save.to.database');
 
-Route::post('/save-to-database', [GeminiController::class, 'saveToDatabase'])->name('save.to.database');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::patch('/tasks/{task}/toggle-status', [TaskController::class, 'toggleStatus'])->name('tasks.toggleStatus');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-Route::patch('/tasks/{task}/toggle-status', [TaskController::class, 'toggleStatus'])->name('tasks.toggleStatus');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    Route::delete('/goals/{goal}', [GoalController::class, 'destroy'])->name('goals.destroy');
 
-Route::delete('/goals/{goal}', [GoalController::class, 'destroy'])->name('goals.destroy');
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
